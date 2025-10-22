162394ec9d3286ac72d22ebe7c607ba80235296d
Harsh Pratap <133808138+harshkunz@users.noreply.github.com>
2025-10-22 16:18:14 +0530

    Update Features.tsx

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -36,19 +36,19 @@ const Features = () => {
   ];
 
   return (
-    <section className="pt-10 pb-14 bg-white">
+    <section className="sm:pt-10 sm:pb-14 pb-6 bg-white">
       <div className="max-w-6xl mx-auto px-6 pt-10 pb-6 text-center">
-        <h2 className="text-3xl font-semibold text-gray-800 mb-10">
+        <h2 className="text-2xl sm:text-3xl font-semibold text-gray-800 mb-6 sm:mb-10">
           Why Choose JobFinder?
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 sm:gap-10">
           {features.map((feature, index) => (
             <div
               key={index}
               className="flex flex-col items-center text-center p-8 border border-gray-200 rounded-3xl shadow-sm bg-white hover:shadow-xl hover:scale-110 hover:-translate-y-1 transition-all duration-300"
             >
-              <div className="mb-4">{feature.icon}</div>
-              <h3 className="text-lg font-bold text-gray-700 mb-2">
+              <div className="mb-3 sm:mb-4">{feature.icon}</div>
+              <h3 className="text-lg font-bold text-gray-700 mb-1 sm:mb-2">
                 {feature.title}
               </h3>
               <p className="text-gray-500 text-xs">{feature.desc}</p>
